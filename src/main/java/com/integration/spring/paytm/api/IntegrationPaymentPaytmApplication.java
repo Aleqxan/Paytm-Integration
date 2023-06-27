b82157f8ec61ebd96de5be3fa796185ccb6feb62
Alex Izuka <inno.alex@ymail.com>
2023-06-27 09:26:28 +0100

    New commit

---
+++ src/main/java/com/integration/spring/paytm/api/IntegrationPaymentPaytmApplication.java
@@ -0,0 +1,13 @@
+package com.integration.spring.paytm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationPaymentPaytmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationPaymentPaytmApplication.class, args);
+    }
+
+}
